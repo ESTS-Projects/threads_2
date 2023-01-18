4a2f89958f77eea97b28ab6ee2ddb81f3a4a28b5
EOussama <oussama_gtacj@live.fr>
2023-01-18 13:44:10 +0100

    Exercice 2

---
+++ src/Exercice2.java
@@ -0,0 +1,27 @@
+class Exercice2 extends Thread {
+    public static String sharedString = "";
+    private String toAppend;
+
+    Exercice2(String toAppend) {
+        this.toAppend = toAppend;
+    }
+
+    public void run() {
+        for (String charString : toAppend.split("")) {
+            if (!isInterrupted()) {
+                    sharedString += charString;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Exercice2 ta = new Exercice2("ABC");
+        Exercice2 tb = new Exercice2("DEF");
+
+        ta.start();
+        tb.start();
+
+        Thread.sleep(1000);
+        System.out.println(sharedString);
+    }
+}
