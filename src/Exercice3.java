53e5aaecd104c9bb7228afdfac39b38e947334a1
EOussama <oussama_gtacj@live.fr>
2023-01-18 13:50:04 +0100

    Exercice 3

--- src/Exercice3.java
+++ src/Exercice3.java
@@ -1,63 +1,51 @@
-class Compte {
-    private int solde = 0;
+class CompteBancaire {
+    private int solde;
 
-    public void ajouter(int somme) {
-        synchronized (this) {
-            solde += somme;
-        }
+    public CompteBancaire() {
+        this.solde = 0;
     }
 
-    public void retirer(int somme) {
-        synchronized (this) {
-            solde = Math.max(0, solde - somme);
-        }
+    public synchronized void deposer(int montant) {
+        this.solde += montant;
     }
 
-    public void operationNulle(int somme) {
-        ajouter(somme);
-        retirer(somme);
+    public synchronized void retirer(int montant) {
+        this.solde -= montant;
     }
 
-    public int getSolde() {
-        return solde;
+    public synchronized int getSolde() {
+        return this.solde;
     }
 }
 
-class Operation extends Thread {
-    private String nomop;
-    private Compte compte;
+class Employe extends Thread {
+    private CompteBancaire compte;
 
-    public Operation(String nom, Compte compte) {
-        this.nomop = nom;
+    public Employe(CompteBancaire compte) {
         this.compte = compte;
     }
 
     public void run() {
-        while (true) {
-            int i = (int) (Math.random() * 10000);
-            String nom = nomop;
-            System.out.println("Nom opération : " + nom);
-            compte.ajouter(i);
-            compte.retirer(i);
-            compte.operationNulle(i);
-            int solde = compte.getSolde();
-            System.out.println(solde);
+        for (int i = 0; i < 10; i++) {
+            compte.deposer(100);
+            System.out.println(this.getName() + " a déposé 100 dollars. Solde : " + compte.getSolde());
 
-            if (solde != 0) {
-                System.out.println(nom + ":*** Solde=" + solde);
-                System.exit(1);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
         }
     }
 }
 
 public class Exercice3 {
-    public static void main(String[] args) throws Exception {
-        Compte compte = new Compte();
-        Operation operation1 = new Operation("Test1", compte);
-        Operation operation2 = new Operation("Test2", compte);
+    public static void main(String[] args) {
+        CompteBancaire compte = new CompteBancaire();
+        Employe employe1 = new Employe(compte);
+        Employe employe2 = new Employe(compte);
 
-        operation1.start();
-        operation2.start();
+        employe1.start();
+        employe2.start();
     }
 }
